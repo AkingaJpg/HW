d35b25786775cfe291df1608974e31a92a267a64
akinga <einarrwolf@gmail.com>
2023-06-11 19:25:41 +0200

    HW_1_edit

--- HW_1.3.js
+++ HW_1.3.js
@@ -1,18 +1,11 @@
 const resultArray = [1, 2, [3, [4]]];
 
-function multiplyArrayElements(array) {
-  let multipliedArray = 1;
+const initialValue = 1;
+const product = resultArray.flat(Infinity).reduce(
+(prev, current) => {
+  return prev * current;
+},
+initialValue
+);
 
-  for (let i = 0; i < array.length; i++) {
-    if (Array.isArray(array[i])) {
-      multipliedArray *= multiplyArrayElements(array[i]);
-    } else {
-      multipliedArray *= array[i];
-    }
-  }
-
-  return multipliedArray;
-}
-
-const productofArray = multiplyArrayElements(resultArray);
-console.log(productofArray);
+console.log(product);
