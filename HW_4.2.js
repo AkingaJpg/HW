21e8c046d1288b3854e7adf45f7eda56efa79c87
Akirktktk <diversefeedback@gmail.com>
2023-07-10 21:22:39 +0200

    deleted 🙀

--- HW_4.2.js
+++ HW_4.2.js
@@ -1,82 +1,54 @@
-function switchToDark() {
-    const { button, text } = getElements();
-    const { time } = getState();
-    let body = document.body;
-
-    button.classList.add("dark-background");
-    body.classList.add("dark-background");
-
-    button.textContent = "Turn on";
-    text.textContent = `Last off time: ${formatDate(time)}`;
-    setState({
-        state: "dark",
-        time: new Date()
-    });
+const button = document.getElementById("myButton");
+const text = document.getElementById("parId");
+let state = localStorage.getItem("state") || "light";
+let time = new Date(localStorage.getItem("time")) ?? new Date()
+if (state === "dark") {
+    switchTo();
 }
 
-function switchToLight() {
-    const { button, text } = getElements();
-    const { time } = getState();
+button.addEventListener('click', switchTo);
 
+function switchTo() {
     let body = document.body;
 
-    button.classList.remove("dark-background");
-    body.classList.remove("dark-background");
+    button.classList.toggle("dark-background");
+    body.classList.toggle("dark-background");
 
-    button.textContent = "Turn off";
-    text.textContent = `Last on time: ${formatDate(time)}`;
-    setState({
-        state: "light",
-        time: new Date()
-    });
+    if (state === "dark") {
+        button.textContent = "Turn off";
+        text.textContent = `Last on time: ${formatDate(time)}`;
+        setState({
+            state: "light",
+            time: new Date()
+        });
+    } else {
+        button.textContent = "Turn on";
+        text.textContent = `Last off time: ${formatDate(time)}`;
+        setState({
+            state: "dark",
+            time: new Date()
+        });
+    }
 }
 
 function formatDate(date) {
-    let day = String(date.getDate()).padStart(2, "0");
-    let month = String(date.getMonth() + 1).padStart(2, "0");
-    let year = date.getFullYear();
-    let hours = String(date.getHours()).padStart(2, "0");
-    let minutes = String(date.getMinutes()).padStart(2, "0");
-    let seconds = String(date.getSeconds()).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
 
     return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 }
 
-function getState() {
-    return {
-        state: localStorage.getItem("state"),
-        time: new Date(localStorage.getItem("time")) ?? new Date()
-    }
+
+
+function setState(currentState) {
+    localStorage.setItem("state", currentState.state);
+    localStorage.setItem("time", currentState.time);
+    time = currentState.time;
+    state = currentState.state;
 }
 
-function setState(state) {
-    localStorage.setItem("state", state.state);
-    localStorage.setItem("time", state.time);
-}
 
-function getElements() {
-    return {
-        button: document.getElementById("myButton"),
-        text: document.getElementById("parId")
-    }
-}
-
-function initState() {
-    const { state } = getState();
-    if (state === "dark") {
-        switchToDark();
-    }
-    else {
-        switchToLight();
-    }
-}
-
-function toggleButton() {
-    console.log("toggle");
-    const { state } = getState();
-    if (state === "dark") {
-        switchToLight();
-    } else {
-        switchToDark();
-    }
-}
